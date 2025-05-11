0ac7a949a76768a3315d6d9ccd4968df4b27cab4
yokobuto <info@yokobuto.de>
2025-05-11 07:43:41 +0200

    Rotation added

--- Photo Viewer/ContentView.swift
+++ Photo Viewer/ContentView.swift
@@ -8,16 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // Dragging
+    @GestureState private var dragAmount = CGSize.zero
+    @State private var offset = CGSize.zero
+    
+    @GestureState private var rotateAmount: Angle = .zero
+    @State private var finalRotation: Angle = .zero
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text("dragAmount width: \(dragAmount.width)")
+            Text("dragAmount height: \(dragAmount.height)").padding(.bottom)
+            Text("offset width: \(offset.width)")
+            Text("offset height: \(offset.height)")
+            
+           
         }
+        .background(.red)
         .padding()
+        .simultaneousGesture(dragGesture)
     }
+    
+    
+    
+    var dragGesture: some Gesture {
+            DragGesture()
+                .updating($dragAmount) { value, state, _ in
+                    state = value.translation
+                }
+                .onEnded { value in
+                    offset = CGSize(
+                        width: offset.width + value.translation.width,
+                        height: offset.height + value.translation.height
+                    )
+                }
+    }
+    
+    var rotationGesture: some Gesture {
+        RotationGesture()
+            .updating($rotateAmount) { value, state, _ in
+                state = value
+            }
+            .onEnded { value in
+                finalRotation += value
+            }
+    }
+    
+    
+    
+    
 }
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
